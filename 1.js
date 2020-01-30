74b392fdde7e990607585d7a4481dba7534b50d1
= <=>
2020-01-30 11:40:03 +0700

    co ban xong file 1.js

---
+++ 1.js
@@ -0,0 +1,17 @@
+﻿ $(function(){
+ 	// xu ly viec click thay doi mau
+ 	$(".tdkhoi").click(function(event) {
+ 		/* Act on the event */
+ 		$(this).toggleClass('xanh');
+
+ 		//khi click vao , cho noi dung hien ra
+ 		$(this).next().slideToggle(900, "easeInOutQuint");
+ 		//su dung ham animate de chuyen noi dung len
+ 		$("body,html").animate({scrollTop: $(this).offset().top},900,"easeInOutQuint");
+ 	});
+ 	//code cho phan phong to anh
+	$(".vaicaianh a").fancybox({openEffect : 'elastic'});//openEffect : 'elastic' la hieu ung cua fancybox co trong file index.html cua file  nen demo trong fancybox, o phan openEffect 
+	//xu ly co dan noi dung accordion
+	$(".ndkhoi").slideUp();
+})  
+ 
